f6484162ed9a7556d089f241eb1e7065f3f2523b
Murphy Zhang Sun <mr_zhangsun@outlook.com>
2019-06-15 14:14:25 +0800

    init

---
+++ src/main/java/site/zhangsun/utility/UtilityApplication.java
@@ -0,0 +1,17 @@
+package site.zhangsun.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Murphy Zhang Sun
+ * @date 2019-06-15
+ */
+@SpringBootApplication
+public class UtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityApplication.class, args);
+    }
+
+}
